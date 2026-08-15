2fdd7a2c7a976a343bed727171c7b8a6b505b444
PriyeshGpta <priyeshgupta27111999@gmail.com>
2026-08-15 17:42:55 +0530

    feat(): add declarative routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App'
 import { AuthProvider } from './app/providers/AuthProvider'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 )
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { Route, Routes } from "react-router";
 import PublicLayout from "../layouts/PublicLayout";
 import Login from "../pages/public/Login";
 import Register from "../pages/public/Register";
@@ -10,59 +10,29 @@ import About from "../pages/private/About";
 import Services from "../pages/private/Services";
 import ContactUs from "../pages/private/ContactUs";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <PublicRoute />,
-        children: [
-            {
-                path: "",
-                element: <PublicLayout />,
-                children: [
-                    {
-                        path: "",
-                        element: <Login />
-                    },
-                    {
-                        path: "register",
-                        element: <Register />
-                    }
-                ]
-            },
-        ],
-    },
-    {
-        path: "/main",
-        element: <PrivateRoute />,
-        children: [
-            {
-                path: "",
-                element: <PrivateLayout />,
-                children: [
-                    {
-                        path: "",
-                        element: <Home />
-                    },
-                    {
-                        path: "about",
-                        element: <About />
-                    },
-                    {
-                        path: "services",
-                        element: <Services />
-                    },
-                    {
-                        path: "contact",
-                        element: <ContactUs />
-                    }
-                ]
-            },
-        ],
-    }
-])
-
 const AppRoutes = () => {
-    return <RouterProvider router={router} />
+    return (
+        <Routes>
+        /* Public Routes */
+            <Route path="/" element={<PublicRoute />}>
+                <Route path="" element={<PublicLayout />}>
+                    <Route path="" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                </Route>
+            </Route>
+
+        /* Private Routes */
+            <Route path="/main" element={<PrivateRoute />}>
+                <Route path="" element={<PrivateLayout />}>
+                    <Route path="" element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="services" element={<Services />} />
+                    <Route path="contact" element={<ContactUs />} />
+                </Route>
+            </Route>
+        </Routes>
+    )
+
 }
 
 export default AppRoutes
