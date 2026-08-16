0aef74b6dfd9a216af1db7549e0254cf4dfff868
PriyeshGpta <priyeshgupta27111999@gmail.com>
2026-08-16 22:07:06 +0530

    chore(useAuth): changed file format

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -5,7 +5,6 @@ import { toast } from "react-toastify";
 import { nanoid } from 'nanoid'
 import { useNavigate } from "react-router";
 
-
 export const useAuth = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
