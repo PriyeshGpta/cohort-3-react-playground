2c5163b54ad78bc6e44a6c99cefc774ee487ae88
PriyeshGpta <priyeshgupta27111999@gmail.com>
2026-08-17 20:42:37 +0530

    refactor(useAuth): remove navigate

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,13 +3,11 @@ import { useForm } from "react-hook-form";
 import { UserContext } from "../app/providers/AuthContextProvider";
 import { toast } from "react-toastify";
 import { nanoid } from 'nanoid'
-import { useNavigate } from "react-router";
 
 export const useAuth = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const { users, setUsers, loggedInUser, setLoggedInUser } = useContext(UserContext);
-    const navigate = useNavigate();
 
     const {
         register,
@@ -29,7 +27,6 @@ export const useAuth = () => {
             const activeUser = { email, password };
             setLoggedInUser(activeUser);
             localStorage.setItem("loggedInUser", JSON.stringify(activeUser));
-            navigate("/main")
             toast.success("Logged in successfully");
         } else {
             toast.error("Invalid Credentials")
