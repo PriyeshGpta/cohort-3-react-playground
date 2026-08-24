cdf34b8decc852ad070bfa066d4cfe4e3102087c
PriyeshGpta <priyeshgupta27111999@gmail.com>
2026-08-24 18:34:12 +0530

    refactor: remove unused vars

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router';
 import { UserContext } from '../app/providers/AuthContextProvider';
 
 const PublicRoute = () => {
-    const { loggedInUser, setLoggedInUser } = useContext(UserContext);
+    const { loggedInUser } = useContext(UserContext);
     if (loggedInUser) return <Navigate to={"/main"} />
     return <Outlet />
 }
