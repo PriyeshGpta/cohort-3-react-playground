d096801311957d4286e83bc4acdf578cd016c80a
PriyeshGpta <priyeshgupta27111999@gmail.com>
2026-08-24 17:56:26 +0530

    refactor(AuthContextProvider): remove ls usage

--- src/app/providers/AuthContextProvider.jsx
+++ src/app/providers/AuthContextProvider.jsx
@@ -5,9 +5,7 @@ export const UserContext = createContext();
 
 // Make a ProviderWrapper which will wrap the entire App, and will share the data across all components.
 export const AuthContextProvider = ({ children }) => {
-    const [loggedInUser, setLoggedInUser] = useState(
-        () => JSON.parse(localStorage.getItem("loggedInUser")) || null
-    );
+    const [loggedInUser, setLoggedInUser] = useState(null);
 
     return (
         <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
