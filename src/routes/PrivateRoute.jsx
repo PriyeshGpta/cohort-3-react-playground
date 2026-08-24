6c4fb2f43da4f3e947a6c0b403cb956075f34b38
PriyeshGpta <priyeshgupta27111999@gmail.com>
2026-08-24 18:36:03 +0530

    refactor: remove unused vars

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router'
 import { UserContext } from '../app/providers/AuthContextProvider';
 
 const PrivateRoute = () => {
-    const { loggedInUser, setLoggedInUser } = useContext(UserContext);
+    const { loggedInUser } = useContext(UserContext);
     if (!loggedInUser) return <Navigate to={"/"} />
     return <Outlet />
 }
